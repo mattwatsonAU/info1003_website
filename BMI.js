24fa6ef1726c605ff1dbebff1b9fb4ae9f2f55a0
Jeremy007 <jeremylike2012@yahoo.com>
2015-10-21 13:12:26 +1100

    Update BMI.js

--- BMI.js
+++ BMI.js
@@ -1,17 +1,17 @@
-Enter file contents herefunction calculate() {
-var weight = document.bmiForm.weight.value
-var height = document.bmiForm.height.value
+function calculate() {
+var weight = document.bmiForm.weight.value;
+var height = document.bmiForm.height.value;
 if(weight > 0 && height > 0){
-var final = weight/(height/100*height/100)
-document.bmiForm.bmi.value = finalBmi
+var final = weight/(height/100*height/100);
+//document.bmiForm.bmi.value = finalBmi
 if(final < 18.5){
-document.bmiForm.meaning.value = "You need eat more."
+document.bmiForm.meaning.value = "You need eat more.";
 }
 if(final > 18.5 && final < 25){
-document.bmiForm.meaning.value = "You have a healthy body."
+document.bmiForm.meaning.value = "You have a healthy body.";
 }
 if(final > 25){
-document.bmiForm.meaning.value = "You are overWeight."
+document.bmiForm.meaning.value = "You are overWeight.";
 }
 }
 else{
